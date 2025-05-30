a4711c1c341e2b4ced7353df7141e382ff9796ad
nluzm <maria.nicoly.2009@aluno.edu.pi.gov.br>
2025-05-30 13:47:00 -0300

    Update aq1.java( sistema escolar)

--- aq1.java
+++ aq1.java
@@ -10,25 +10,25 @@ public class SistemaEscolar {
         double[] mediasBimestrais = new double[4];
         double[] mediasSemestrais = new double[2];
 
-        // Entrada das 8 notas
+        
         System.out.println("Digite as 8 notas do ano:");
         for (int i = 0; i < 8; i++) {
             System.out.print("Nota " + (i + 1) + ": ");
             notas[i] = scanner.nextDouble();
         }
 
-        // Cálculo das médias bimestrais
+        
         for (int i = 0; i < 4; i++) {
             mediasBimestrais[i] = (notas[i * 2] + notas[i * 2 + 1]) / 2;
         }
 
-        // Cálculo das médias semestrais
+       
         mediasSemestrais[0] = (mediasBimestrais[0] + mediasBimestrais[1]) / 2;
         mediasSemestrais[1] = (mediasBimestrais[2] + mediasBimestrais[3]) / 2;
 
-        // Cálculo da média final
+       
         double mediaFinal = (mediasSemestrais[0] + mediasSemestrais[1]) / 2;
 
-        // Saída formatada dos resultados
+        
         {
 }
